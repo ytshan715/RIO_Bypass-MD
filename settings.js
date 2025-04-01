79fd4e9d624833fd9e220ce1acb3dd975d1d9072
ytshan715 <bandarashanuka933@gmail.com>
2025-04-01 22:01:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+    return text === fault ? true : false;true
 }
 module.exports = {
 
